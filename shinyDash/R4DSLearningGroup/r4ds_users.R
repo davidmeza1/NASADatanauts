1aed26ff117fa6ee1fa822870b721b51b80743a4
David Meza <david.meza-1@nasa.gov>
2018-01-03 11:19:14 -0600

    Learning Group
    Added map of learning group

---
+++ shinyDash/R4DSLearningGroup/r4ds_users.R
@@ -0,0 +1,35 @@
+library(googlesheets)
+library(dplyr)
+library(ggmap)
+library(leaflet)
+library(glue)
+# Authenticate your google account and save token
+ttt <- gs_auth()
+saveRDS(ttt, "ttt.rds")
+# Get sheet key and read Sheet1
+r4ds_deidentified <- gs_url("https://docs.google.com/spreadsheets/d/1L7lU5QqVfUtZXenFsGErPFS4n1jL4fixx9d3lrF94hU/edit#gid=0")
+r4ds_users <- r4ds_deidentified %>% gs_read("Sheet1")
+# Change names to something easier to use
+names(r4ds_users) <- c("Type", "R_Comfort", "Analysis_Comfort", "Location", "Cohort")
+# filter out the rows missing a location
+r4ds_users <- r4ds_users %>%
+  filter(!is.na(Location))
+# Add longitude and lattitude to the dataframe, using Data Science Toolkit since
+# it has no rate limit
+r4ds_users <- bind_cols(r4ds_users, data.frame(geocode(r4ds_users$Location, source = "dsk")))
+# filter out rows with not coordinates
+r4ds_users <- r4ds_users %>%  filter(!is.na(lon))
+save(r4ds_users, file = "r4dsUsers.rdata")
+
+# load("~/Google Drive/MezaEdD/r4dsUsers.rdata")
+
+# Create leaflet map and popup to identify users comfort in R and analysis
+r4ds_map <- leaflet() %>%
+  addTiles() %>%
+  addCircleMarkers(data = r4ds_users, lng = ~ lon, lat = ~ lat,
+                   popup = glue('My R Comnfort is {r4ds_users$R_Comfort} ,', ' My Analysis Comfort is {r4ds_users$Analysis_Comfort}'))
+
+r4ds_map
+
+# To Do
+# add layer for learner and mentor groiuped by cohort

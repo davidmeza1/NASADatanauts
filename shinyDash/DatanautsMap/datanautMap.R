1e7623abc3faacc9b9581da535d3ab403d6396ac
davidmeza1 <david.meza-1@nasa.gov>
2018-02-14 16:34:51 -0500

    update datanaut map R script with comments

--- shinyDash/DatanautsMap/datanautMap.R
+++ shinyDash/DatanautsMap/datanautMap.R
@@ -13,17 +13,27 @@ library(crosstalk)
 ttt <- gs_auth()
 saveRDS(ttt, "ttt.rds")
 
+#Create an object to store the url to the googlesheet containing the information
 datanautsLocation <- gs_url("https://docs.google.com/spreadsheets/d/1aDqpgXzxOQY6D8uQa8TNA2Xef09wRyaqUbVT7yLc-F4/edit#gid=1242744883")
-datanauts <- datanautsLocation %>% gs_read("Form Responses 1")                            
+# Read in the sheet
+datanauts <- datanautsLocation %>% gs_read("Form Responses 1")
+# Renaming columns to something easier to use
 names(datanauts) <- c("Timestamp", "name", "city", "state", "country", "industry", "class", "share")
 
+# Pasting city, state and country into one column
+# Then I am removing the NA from rows with datanauts not in US
+# Binding colums from datanauts df and the lot, lan from geocode pull using ggmap
+# Converting class to factor
 datanauts <- datanauts %>%  mutate(location = paste0(city, ", ", state, ", ", country))
 datanauts <- datanauts %>%  mutate(location = str_replace(location, "NA, ", ""))
 datanauts <- bind_cols(datanauts, data.frame(geocode(datanauts$location, source = "dsk")))
 datanauts$class <- factor(datanauts$class)
-
+# Save the rdata file for use later
 save(datanauts, file = "datanauts.Rdata")
+# Do not run
 load("datanauts.Rdata")
+
+# Thsi creates an object holding a leaflet map
 datanauts_map <- leaflet() %>%
     setView(-82.831040,35.88904, zoom = 3) %>% 
     addTiles() %>%
@@ -40,12 +50,15 @@ datanauts_map <- leaflet() %>%
                      options = layersControlOptions(collapsed = FALSE))
 
 
-# Creates datatable of the Datanauts
+# Creates datatable of the Datanauts using only 4 columns
 datatable(datanauts %>% select(name, location, industry, class))
 
 # Create subset to separate industry
-industry_levels <- c("Finance", "Academia", "Oil & Gas", "Non-profit", "Government", "Natural/Physical Science and Engineering",
-                     "Computer Science", "Student", "Education", "Bioinformatics", "Scocial Science", "Other")
+# Did not use this
+#industry_levels <- c("Finance", "Academia", "Oil & Gas", "Non-profit", "Government", "Natural/Physical Science and Engineering",
+#                     "Computer Science", "Student", "Education", "Bioinformatics", "Scocial Science", "Other")
+
+# Creates subset of the member df for is on the wordcloud
 datanaut_sub <- datanauts %>% select(name, location, industry, class)
 datanaut_sub <- datanaut_sub %>% separate_rows(industry, sep = ",")
 datanaut_sub$industry <- str_trim(datanaut_sub$industry)
@@ -53,11 +66,11 @@ datanaut_sub$industry <- factor(datanaut_sub$industry)
 
 industry_count <- fct_count(datanaut_sub$industry, sort = TRUE)
 
-pal <- c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#fb8072')
-wordcloud(industry_count$f, industry_count$n, scale=c(3,.5), min.freq = 1, colors = pal)
-
 library(wordcloud2)
 wordcloud2(industry_count, size = .5, color = "random-light", backgroundColor = "grey")
+
+# Could also have done
+# wordcloud2(fct_count(datanaut_sub$industry, sort = TRUE), size = .5, color = "random-light", backgroundColor = "grey")
 
 
 
